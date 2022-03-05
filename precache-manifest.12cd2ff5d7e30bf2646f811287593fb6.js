2ff70cd0d332eaeba69e51f405e71cd8b68919c8
JXM <1162782002@qq.com>
2022-03-05 16:51:27 +0800

    new deploy

---
+++ precache-manifest.12cd2ff5d7e30bf2646f811287593fb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146c7e5abea669bd3f73",
+    "url": "/Acount-page/css/app.1cc8ab2e.css"
+  },
+  {
+    "revision": "9cb3ea564f27d2746917609b903963af",
+    "url": "/Acount-page/index.html"
+  },
+  {
+    "revision": "146c7e5abea669bd3f73",
+    "url": "/Acount-page/js/app.c9c1c948.js"
+  },
+  {
+    "revision": "3d411c986547c235dac4",
+    "url": "/Acount-page/js/chunk-vendors.e67f26e2.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/Acount-page/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Acount-page/robots.txt"
+  }
+]);
